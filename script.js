63427ee777dfad8fb09ab8faa925ed7503f5f6f2
helenhosse <helenhosse@gmail.com>
2022-11-09 18:53:54 -0700

    added containers to js

---
+++ script.js
@@ -0,0 +1,7 @@
+var timerEl = document.getElementById('timer');
+
+var startEl = document.getElementById('start-quiz')
+var quizEl = document.getElementById('quiz-container')
+var questionContainerEl = document.getElementById('question-container')
+var answerButtonsEl = document.getElementById('answer-buttons')
+var questionDisplay = document.getElementById('question-display')
